4fbe5721199e8fe0de8ec1e1d92bdf249de00a02
Ahmedma1her <ahmedma1her@gmail.com>
2026-06-08 12:38:37 +0300

    adding subnet , igw, igw assoc

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,46 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
+
   tags = {
-    name : var.vpc_name
+    Name = var.vpc_name
   }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.route_table_name
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,23 @@
 variable "vpc_name" {
   type = string
 }
+
 variable "cidr_block" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+variable "igw_name" {
+  type = string
+}
+
+variable "route_table_name" {
   type = string
 }
